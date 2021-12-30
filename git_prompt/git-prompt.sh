f2ee7a6d7398d1b6d6fef6045f9665b66bf4e24e
Daniel Leal <danielleal94@gmail.com>
2021-12-30 09:23:21 -0300

    Update git-prompt.sh

--- git_prompt/git-prompt.sh
+++ git_prompt/git-prompt.sh
@@ -11,14 +11,18 @@ if test -f ~/.config/git/git-prompt.sh
 then
 	. ~/.config/git/git-prompt.sh
 else
-	PS1='\[\033]0;☺ Working ☺\007\]' # set window title
+	PS1='\[\033]0;Working\007\]'   # set window title
 	PS1="$PS1"'\n'                 # new line
 	PS1="$PS1"'\[\033[32m\]'       # change to green
 	PS1="$PS1"'NooÐe '             # user@host<space>
 	PS1="$PS1"'\[\033[35m\]'       # change to purple
-	# PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
+	# PS1="$PS1"'$MSYSTEM '        # show MSYSTEM
+	PS1="$PS1"'\[\033[37m\]'       # change to white
+	PS1="$PS1"'in '                # in message
 	PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
-	PS1="$PS1"'- [\W]'                 # current working directory
+	PS1="$PS1"'[\W]'               # Directory
+	PS1="$PS1"'\[\033[31m\]'       # change to brownish red
+	PS1="$PS1"' \t'                # DATE
 	if test -z "$WINELOADERNOEXEC"
 	then
 		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
